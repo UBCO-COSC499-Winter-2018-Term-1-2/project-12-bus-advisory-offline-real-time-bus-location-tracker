8b24f9021996f5726daedbe45ebc817606da9f5b
mattds9 <mattdeleeuw@hotmail.com>
2019-01-08 16:40:48 -0800

    fixed

---
+++ development/app/src/main/java/com/fremtidmedia/httpwww/busadvisory/MapActivity.java
@@ -0,0 +1,190 @@
+package com.fremtidmedia.httpwww.busadvisory;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.Image;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.common.PositioningManager;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapFragment;
+import com.here.android.mpa.common.GeoPosition;
+import com.here.android.mpa.mapping.MapMarker;
+import com.here.android.mpa.mapping.MapState;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
+import com.kontakt.sdk.android.common.KontaktSDK;
+import com.kontakt.sdk.android.common.profile.IBeaconDevice;
+import com.kontakt.sdk.android.common.profile.IBeaconRegion;
+
+public class MapActivity extends Activity {
+
+    public void clickTrack(View views) {
+
+        Log.i("Info", "TRACK pressed");
+    }
+
+    public void clickBusNum(View views) {
+
+        Log.i("Info", "BusNum pressed");
+    }
+
+    public void clickExit(View views) {
+
+        Log.i("Info", "clickExit pressed");
+    }
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    IBeaconDevice searchBeacon;
+
+    // map embedded in the map fragment
+    private Map map = null;
+
+    // map fragment embedded in this activity
+    private MapFragment mapFragment = null;
+
+
+//
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(MapActivity.this, new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String newToken = instanceIdResult.getToken();
+                Log.e("This Token", newToken);
+            }
+        });
+        initialize();
+        KontaktSDK.initialize("zwPcatzTlLvusdiKXJKImhTqqhVbAJyN");
+        kontaktDetect();
+
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+//                Toast.makeText(MapActivity.this, Double.toString(location.getLatitude()) + ", " + Double.toString(location.getLongitude()) , Toast.LENGTH_SHORT).show();
+
+                //Active tracking code being worked on:
+                //map = mapFragment.getMap();
+                // map.setCenter(new GeoCoordinate(location.getLatitude(),location.getLongitude()), Map.Animation.NONE);
+                //createMapMarker();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+            }
+        }
+
+
+
+    private void initialize() {
+        setContentView(R.layout.activity_map);
+        // Search for the map fragment to finish setup by calling init().
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
+
+        mapFragment.init(new OnEngineInitListener() {
+            @Override
+            public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+                if (error == OnEngineInitListener.Error.NONE) {
+                    // retrieve a reference of the map from the map fragment
+                    map = mapFragment.getMap();
+                    Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    map.setCenter(new GeoCoordinate(loc.getLatitude(), loc.getLongitude(), 0.0),
+                            Map.Animation.NONE);
+                    createMapMarker();
+
+                    // Set the zoom level to the average between min and max
+                    map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+                } else {
+                    System.out.println("ERROR: Cannot initialize Map Fragment");
+                }
+            }
+        });
+    }
+
+    private void createMapMarker() {
+        Image marker_img = new Image();
+        try {
+            marker_img.setImageResource(R.drawable.iconfinder_map_marker_299087);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        map = mapFragment.getMap();
+        MapMarker marker = new MapMarker(map.getCenter(), marker_img);
+        map.addMapObject(marker);
+
+    }
+
+    private void kontaktDetect() {
+        IBeaconListener iBeaconListener = new SimpleIBeaconListener() {
+            @Override
+            public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
+                Log.e("beacon", ibeacon.toString());
+                if(ibeacon.equals(searchBeacon)){
+
+                }
+            }
+        };
+    }
+}
