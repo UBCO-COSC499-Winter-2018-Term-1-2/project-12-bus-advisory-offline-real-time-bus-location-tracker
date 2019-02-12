75933fc2c42232ef2f94cfd969cf987c6cd720be
Wasek <wesinem@gmail.com>
2019-02-11 23:39:31 -0800

    /POST for receiving bus location

--- development/rest-apis/app.js
+++ development/rest-apis/app.js
@@ -14,6 +14,7 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Trigger} = require('./models/trigger');
+var {BusLocation} = require('./models/busLocation');
 
 var app = express();
 
@@ -38,10 +39,27 @@ app.post('/triggers', (req, res) => {
     });
 });
 
+app.post('/buslocation', (req, res) => {
+    var busLocation = new BusLocation({
+        busId: req.body.busId,
+        location: {
+            type: req.body.location.type,
+            coordinates: req.body.location.coordinates
+        },
+        "timestamp": new Date().getTime()
+    });
+
+    busLocation.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 //app.listen(3000, () => {
 //    console.log('Started on port 3000');
 //});
-//
+
 module.exports = {app};
 
 //module.exports.handler = serverless(app);
---
+++ development/rest-apis/models/busLocation.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var BusLocation = mongoose.model('BusLocation', {
+    busId: {
+        type: String
+    },
+    location: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
+    timestamp:Number
+});
+
+module.exports = {BusLocation}
