021da5641d0263ca6681644b57b7999b6cc6e6ef
Wasek <wesinem@gmail.com>
2019-02-19 21:57:18 -0800

    get APIs for buslocation and triprequest

--- development/rest-apis/app.js
+++ development/rest-apis/app.js
@@ -75,6 +75,41 @@ app.post('/triprequest', (req, res) => {
     });
 });
 
+app.get('/buslocation', (req, res) => {
+  BusLocation.find().then((buslocations) => {
+    res.send({buslocations});
+    console.log(buslocations);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/triprequest', (req, res) => {
+  TripRequest.find().then((tripreqs) => {
+    res.send({tripreqs});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+//app.get('/todos/:id', (req, res) => {
+//  var id = req.params.id;
+//
+//  if (!ObjectID.isValid(id)) {
+//    return res.status(404).send();
+//  }
+//
+//  Todo.findById(id).then((todo) => {
+//    if (!todo) {
+//      return res.status(404).send();
+//    }
+//
+//    res.send({todo});
+//  }).catch((e) => {
+//    res.status(400).send();
+//  });
+//});
+
 
 //app.listen(3000, () => {
 //    console.log('Started on port 3000');
