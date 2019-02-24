6dea20cd465bc80cca33c5f05a897324ac9b8faf
mattds9 <mattdeleeuw@hotmail.com>
2019-02-23 18:02:06 -0800

    added firebase messaging triggers

--- development/rest-apis/here_maps/config.js
+++ development/rest-apis/here_maps/config.js
@@ -1,7 +1,16 @@
-var config = {
+var here_maps_config = {
     app_id: 'B8XsbbQTez6ZoBejQzCC',
     app_code: 'fWvulY1Z0dj0d1nPvF1oVA'
 };
+var firebaseConfig = {
+    apiKey: "AIzaSyBibwa5zLw14GkjLekXsnisMM3Z7xbccQ8",
+    authDomain: "bus-advisory.firebaseapp.com",
+    databaseURL: "https://bus-advisory.firebaseio.com",
+    projectId: "bus-advisory",
+    storageBucket: "bus-advisory.appspot.com",
+    messagingSenderId: "691626187941"
+  };
 module.exports = {
-    config
+    config: here_maps_config,
+    firebaseConfig
 }
--- development/rest-apis/here_maps/here_maps_trigger.js
+++ development/rest-apis/here_maps/here_maps_trigger.js
@@ -1,11 +1,14 @@
-const confi = require('./config');
-const hm = require('./here_maps_wrapper');
+const request = require('request');
+const firebase = require('firebase-admin');
 
+const config = require('./config');
+const hm = require('./here_maps_wrapper');
+firebase.initializeApp(config.firebaseConfig);
 
 
 var getTime = (startLatLng, endLatLng) => {
 return new Promise((resolve, reject) => {
-    hm.configure(confi.config).then(() => {
+    hm.configure(config.here_maps_config).then(() => {
 hm.requestSimpleString(startLatLng, endLatLng).then(
     (body) => {
         resolve(body.response.route[0].summary.trafficTime);
@@ -16,15 +19,37 @@ hm.requestSimpleString(startLatLng, endLatLng).then(
 });
 };
 
+var sendRequest = (startLatLng, endLatLng, busStopName) => {
+    return new Promise((resolve, reject) => {
+        var time = Math.round(getTime(startLatLng, endLatLng)/60);
+        if( time <= 100 ) {
+            var message = {
+                notification: {
+                  title: `Your Bus is Arriving Soon`,
+                  body: `Your Bus is arriving in approximately ${time} minutes`
+                },
+                topic: `${busStopName}${time}`
+              };
+              firebase.messaging.send(message).then((response) => {
+                // Response is a message ID string.
+                console.log('Successfully sent message:', response);
+              })
+              .catch((error) => {
+                console.log('Error sending message:', error);
+              });
+        }
+    });
+};
 
-getTime("49.9399807,-119.395521", "49.9081381,-119.3917857").then((trafficTime) => {
-    console.log(trafficTime);
-}, (err) => {
-    console.log(err)
-});
+
+// getTime("49.9399807,-119.395521", "49.9081381,-119.3917857").then((trafficTime) => {
+//     console.log(trafficTime);
+// }, (err) => {
+//     console.log(err)
+// });
 
 
 
 module.exports = {
-    getTime
+
 }
