0eb37e4528c692b0407e7cbcc7b5ae4e6840aed0
Wasek <wesinem@gmail.com>
2019-02-24 01:43:47 -0800

    PATCH api for triggers

--- development/rest-apis/models/trigger.js
+++ development/rest-apis/models/trigger.js
@@ -12,6 +12,39 @@ var Trigger = mongoose.model('Trigger', {
         required: true,
         trim: true,
         minlength: 1
+    },
+    busId: {
+        type: String
+    },
+    startedAt: {
+        type: Date,
+        required: true
+    },
+    completedAt: {
+        type: Date,
+        default: null
+    },
+    startLocation: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    endLocation: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            default: 'Point'
+        },
+        coordinates: {
+            type: [Number],
+            default: null
+        }
     }
 });
 
