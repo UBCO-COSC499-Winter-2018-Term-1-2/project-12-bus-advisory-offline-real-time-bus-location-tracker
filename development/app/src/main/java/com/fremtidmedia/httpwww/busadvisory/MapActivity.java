6a896290b0979c24dd6d6891ed6f55dfbbacf545
mattds9 <mattdeleeuw@hotmail.com>
2019-03-15 17:37:22 -0700

    deleted useless code

--- development/app/src/main/java/com/fremtidmedia/httpwww/busadvisory/MapActivity.java
+++ development/app/src/main/java/com/fremtidmedia/httpwww/busadvisory/MapActivity.java
@@ -3,41 +3,24 @@ package com.fremtidmedia.httpwww.busadvisory;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.util.TypedValue;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import android.content.DialogInterface;
-import android.app.AlertDialog.Builder;
 import android.app.AlertDialog;
-
-
-import android.content.Context;
-import android.graphics.*;
-import android.graphics.drawable.Drawable;
-import android.text.InputType;
-import android.util.AttributeSet;
-import android.util.TypedValue;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.NumberPicker;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -45,17 +28,12 @@ import com.google.firebase.iid.InstanceIdResult;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.Image;
 import com.here.android.mpa.common.OnEngineInitListener;
-import com.here.android.mpa.common.PositioningManager;
-import com.here.android.mpa.common.ViewObject;
 import com.here.android.mpa.mapping.Map;
 import com.here.android.mpa.mapping.MapFragment;
-import com.here.android.mpa.common.GeoPosition;
 import com.here.android.mpa.mapping.MapMarker;
 import com.here.android.mpa.mapping.MapObject;
-import com.here.android.mpa.mapping.MapState;
 
 import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -65,117 +43,19 @@ import com.kontakt.sdk.android.common.KontaktSDK;
 import com.kontakt.sdk.android.common.profile.IBeaconDevice;
 import com.kontakt.sdk.android.common.profile.IBeaconRegion;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
-import android.app.TimePickerDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.widget.NumberPicker;
-import android.widget.TimePicker;
-
 import biz.kasual.materialnumberpicker.MaterialNumberPicker;
 
-import static com.here.android.mpa.internal.r.H;
-
 public class MapActivity extends Activity {
-
-    private TimePicker time_picker;
-
-    private Button button_set_time;
-
-
-
-
-
-
-
-
-
-
-   // FloatingActionButton FABexit = (FloatingActionButton) findViewById(R.id.FAB_exit);
-
-   // FABexit.setOnClickListener(new View.OnClickListener()
-
-
-
-// Buttons
-
-    Button exitButton;
-   // Button trackButton;
-    Button fiveButton;
-    Button tenButton;
-    Button fifteenButton;
-    Button okButton;
-
-
-
-
-// TextView
-
-    TextView ETAmenu;
-
-
-
-
-
-
-
-
-
-
-    public void clickTrack(View views) {
-
-        Log.i("Info", "TRACK pressed");
-
-
-        // Making the ETA menu visible
-        fiveButton.setVisibility(View.VISIBLE);
-
-        tenButton.setVisibility(View.VISIBLE);
-
-        fifteenButton.setVisibility(View.VISIBLE);
-
-        ETAmenu.setVisibility(View.VISIBLE);
-
-
-        // Making the exit button visible
-        exitButton.setVisibility(View.VISIBLE);
-    }
-
-    public void clickBusNum(View views) {
-
-        Log.i("Info", "BusNum pressed");
-
-        exitButton.setVisibility(View.VISIBLE);
-
-        // NEW ACTIVITY METHOD
-       // Intent intent = new Intent(this, );
-        //startActivity(intent);
-       // openBusNumAc();
-    }
-
-
-
-
-    // Button methods
 
     LocationManager locationManager;
     LocationListener locationListener;
     IBeaconDevice searchBeacon;
 
-    // map embedded in the map fragment
     private Map map = null;
 
-    // map fragment embedded in this activity
     private MapFragment mapFragment = null;
 
     List<MapObject> objList = new ArrayList<>();
-
-
-//
-
 
 
     @Override
@@ -250,18 +130,6 @@ public class MapActivity extends Activity {
         anim.setRepeatMode(Animation.REVERSE);
         anim.setRepeatCount(Animation.INFINITE);
 
-
-
-
-
-
-
-
-
-
-        // FAB Buttons onClicks & Number Picker
-
-        //EXIT ()
         final FloatingActionButton fabEXIT = findViewById(R.id.floatingActionButtonEXIT);
         fabEXIT.hide();
         fabEXIT.setOnClickListener(new View.OnClickListener() {
@@ -271,8 +139,6 @@ public class MapActivity extends Activity {
                 TRACKING.clearAnimation();
                 TRACKING.setVisibility(View.INVISIBLE);
                 fabEXIT.hide();
-                //fabGO.show(); <- tears, for some reason this isn't working...
-                // i think itds because the function is undeneath it
 
             }
         });
@@ -286,7 +152,6 @@ public class MapActivity extends Activity {
         numberPicker.setSeparatorColor(Color.TRANSPARENT);
         numberPicker.setTextColor(Color.BLACK);
         numberPicker.setTextSize(50);
-        //numberPicker.isFocusabilityEnabled();
         numberPicker.setWrapSelectorWheel(true);
         numberPicker.buildLayer();
 
@@ -306,16 +171,12 @@ public class MapActivity extends Activity {
             }
         });
 
-        //GO BUTTON (this function is for the GO button... the methods go in here)
         final FloatingActionButton fabGO = findViewById(R.id.newGO
         );
         fabGO.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i("Info", "GO pressed");
-                //fabEXIT.show();
-               // TRACKING.setVisibility(View.VISIBLE);
-               // TRACKING.startAnimation(anim);
                 newAL.create().show();
 
 
@@ -323,7 +184,6 @@ public class MapActivity extends Activity {
             }
         });
 
-        //BUSNUM BUTTON (the functions for the BUSNUM GO HERE)
         FloatingActionButton fabBusNum = findViewById(R.id.newBusNum);
         fabBusNum.setOnClickListener(new View.OnClickListener() {
             @Override
