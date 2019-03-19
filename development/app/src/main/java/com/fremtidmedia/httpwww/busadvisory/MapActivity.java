24a31d9caf12b9513d6cae1f551242142aad8b68
mattds9 <mattdeleeuw@hotmail.com>
2019-03-19 15:37:48 -0700

    Revert "Merge pull request #45 from UBCO-COSC499-Winter-2018-Term-1-2/arrival-time"
    This reverts commit 4cc492d60807cd23e1f6a8e23aad2d1e0273fbea, reversing
    changes made to 0aa271d2e60099a7bce32519741e7cda1636523c.

--- development/app/src/main/java/com/fremtidmedia/httpwww/busadvisory/MapActivity.java
+++ development/app/src/main/java/com/fremtidmedia/httpwww/busadvisory/MapActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
@@ -61,13 +62,6 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.here.android.mpa.mapping.MapRoute;
-import com.here.android.mpa.routing.Route;
-import com.here.android.mpa.routing.RouteManager;
-import com.here.android.mpa.routing.RouteOptions;
-import com.here.android.mpa.routing.RoutePlan;
-import com.here.android.mpa.routing.RouteResult;
-import com.here.android.mpa.routing.RouteTta;
 import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener;
 import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
 import com.kontakt.sdk.android.common.KontaktSDK;
@@ -94,10 +88,8 @@ public class MapActivity extends Activity {
     private GeoCoordinate busLocation;
     private PositioningManager positioningManager = null;
     private PositioningManager.OnPositionChangedListener positionListener;
-    private MapRoute m_mapRoute;
-    private int arrTime;
-    Timer timer = null;
-    BusTask timerTask = null;
+    Timer t = null;
+    BusTask tt = null;
 
 
 
@@ -109,10 +101,13 @@ public class MapActivity extends Activity {
     String id;
 
     public void testBus(View views){
+      /*  t = new Timer();
+        tt = new BusTask();
+        t.schedule(tt, 0, 5000);
         TextView t3 = findViewById(R.id.textView3);
         t3.setClickable(false);
         centerView(busLocation);
-
+*/
 
     }
 
@@ -140,11 +135,12 @@ public class MapActivity extends Activity {
 
     public void onDestroy() {
         if (positioningManager != null) {
+            // Cleanup
             positioningManager.removeListener(
                     positionListener);
         }
-        timerTask.cancel();
-        timer.cancel();
+        tt.cancel();
+        t.cancel();
         map = null;
         super.onDestroy();
     }
@@ -199,43 +195,6 @@ public class MapActivity extends Activity {
             queue.add(jsonObjectRequest);
         }
 
-    public void initialize(String url){
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        try {
-                            JSONArray location = response.getJSONArray("buslocation");
-                            JSONObject object1 = location.getJSONObject(0);
-                            JSONObject object2 = object1.getJSONObject("location");
-                            JSONArray object3 = object2.getJSONArray("coordinates");
-                            String lon = object3.getString(0);
-                            String lat = object3.getString(1);
-                            busLocation = new GeoCoordinate(Double.parseDouble(lat), Double.parseDouble(lon) );
-                            Log.d("Location", busLocation.getLatitude() + ", " +  busLocation.getLongitude());
-                            timer = new Timer();
-                            timerTask = new BusTask();
-                            timer.schedule(timerTask, 0, 5000);
-                        }
-                        catch (Exception e){
-
-                            Log.e("HERE", "Caught: " + e.getMessage());
-
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-
-                    }
-                });
-        queue.add(jsonObjectRequest);
-    }
-
 
 
     @Override
@@ -267,6 +226,7 @@ public class MapActivity extends Activity {
         network = new BasicNetwork(new HurlStack());
         queue = Volley.newRequestQueue(this);
         queue.start();
+        makeGetRequest("https://oyojktxw02.execute-api.us-east-1.amazonaws.com/dev/buslocation");
         id = FirebaseInstanceId.getInstance().getInstanceId().toString();
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(MapActivity.this, new OnSuccessListener<InstanceIdResult>() {
             @Override
@@ -300,7 +260,6 @@ public class MapActivity extends Activity {
                         Image userImage = new Image();
                         userImage.setImageResource(R.drawable.ic_action_person_pin);
                         map.getPositionIndicator().setMarker(userImage);
-                        initialize("https://oyojktxw02.execute-api.us-east-1.amazonaws.com/dev/buslocation");
 
                         busStops.add(stop1);
                         busStops.add(stop2);
@@ -342,7 +301,7 @@ public class MapActivity extends Activity {
 
 
 
-        //TRACKING & its animation
+        //TRACKING & its animation (might not use)
         final TextView TRACKING = findViewById(R.id.tracking);
         TRACKING.setVisibility(View.INVISIBLE);
 
@@ -352,73 +311,121 @@ public class MapActivity extends Activity {
         anim.setRepeatMode(Animation.REVERSE);
         anim.setRepeatCount(Animation.INFINITE);
 
+        final TextView GoText = findViewById(R.id.GoText);
+        GoText.setVisibility(View.INVISIBLE);
+        final FloatingActionButton fabGO = findViewById(R.id.newGO);
+        fabGO.hide();
+
+
         final FloatingActionButton fabEXIT = findViewById(R.id.floatingActionButtonEXIT);
         fabEXIT.hide();
         fabEXIT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i("Info", "Exit pressed");
-                TRACKING.clearAnimation();
-                TRACKING.setVisibility(View.INVISIBLE);
-                fabEXIT.hide();
-                if(!markerList.isEmpty()) {
-                    map.removeMapObjects(markerList);
-                    markerList.clear();
-                }
-                TextView t3 = findViewById(R.id.textView3);
-                t3.setClickable(true);
+
+                //ARE U SURE ALERT
+                AlertDialog.Builder builder = new AlertDialog.Builder(MapActivity.this);
+                builder.setTitle("Confirmation");
+                builder.setMessage("Are you sure you want to cancel your reminder?");
+
+                builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        fabEXIT.hide();
+                        if(!markerList.isEmpty()) {
+                            map.removeMapObjects(markerList);
+                            markerList.clear();
+                        }
+                     //   tt.cancel();
+                      //  t.cancel();
+                        TextView t3 = findViewById(R.id.BottomBAR);
+                        t3.setClickable(true);
+
+                        fabGO.hide();
+                        GoText.setVisibility(View.INVISIBLE);
+
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog al = builder.create();
+                al.show();
+
+
+
+
 
             }
         });
 
-        //NUMBER PICKER
-        final MaterialNumberPicker numberPicker = new MaterialNumberPicker(this);
-
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(100);
-        numberPicker.setBackgroundColor(Color.WHITE);
-        numberPicker.setSeparatorColor(Color.TRANSPARENT);
-        numberPicker.setTextColor(Color.BLACK);
-        numberPicker.setTextSize(50);
-        numberPicker.setWrapSelectorWheel(true);
-        numberPicker.buildLayer();
-
-        final AlertDialog.Builder newAL = new AlertDialog.Builder(this);
-
-        newAL.setTitle("How much time?");
-        newAL.setView(numberPicker);
-
-        newAL.setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                fabEXIT.show();
-                TRACKING.setVisibility(View.VISIBLE);
-                TRACKING.startAnimation(anim);
 
 
-            }
-        });
 
-        final FloatingActionButton fabGO = findViewById(R.id.newGO
-        );
+
+
         fabGO.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i("Info", "GO pressed");
+
+                //NUMBER PICKER
+                MaterialNumberPicker numberPicker = new MaterialNumberPicker(MapActivity.this);
+
+                numberPicker.setMinValue(1);
+                numberPicker.setMaxValue(100);
+                numberPicker.setBackgroundColor(Color.WHITE);
+                numberPicker.setSeparatorColor(Color.TRANSPARENT);
+                numberPicker.setTextColor(Color.BLACK);
+                numberPicker.setTextSize(50);
+                numberPicker.setWrapSelectorWheel(true);
+                numberPicker.buildLayer();
+
+                final AlertDialog.Builder newAL = new AlertDialog.Builder(MapActivity.this);
+
+                newAL.setTitle("Remind me before the bus arrival \n (in minutes) at my stop");
+                newAL.setView(numberPicker);
+
+                newAL.setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        fabEXIT.show();
+                        fabGO.hide();
+
+                    }
+                });
+
                 newAL.create().show();
 
 
-
             }
         });
 
-        FloatingActionButton fabBusNum = findViewById(R.id.newBusNum);
-        fabBusNum.setOnClickListener(new View.OnClickListener() {
+        TextView t0 = findViewById(R.id.textView97);
+        final TextView BottomBar = findViewById(R.id.BottomBAR);
+
+        BottomBar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Log.i("Info", "GO pressed");
+            public void onClick(View v) {
+                fabGO.show();
+                GoText.setVisibility(View.VISIBLE);
+                t = new Timer();
+                tt = new BusTask();
+                t.schedule(tt, 0, 5000);
+                BottomBar.setClickable(false);
+                centerView(busLocation);
             }
         });
+
 
 
         }
@@ -430,15 +437,27 @@ public class MapActivity extends Activity {
         @Override
          public void run() {
             makeGetRequest("https://oyojktxw02.execute-api.us-east-1.amazonaws.com/dev/buslocation");
-            int time = arrivalEst()/60;
-            if (time != 0 &&  time >= 60) {
-                Log.d("kyle", Integer.toString(time));
-            }else if(time < 60) {
-                Log.d("kyle", "<1 minute");
-            }
             createBus(busLocation);
             Log.d("HERE", "Bus location updated");
         }
+    }
+
+
+    public void createStops(GeoCoordinate location) {
+        Image marker_img = new Image();
+        try {
+            marker_img.setImageResource(R.drawable.bus_stop);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        map = mapFragment.getMap();
+        MapMarker stop1 = new MapMarker(location, marker_img);
+        busStops.add(stop1);
+        map.addMapObject(stop1);
+        MapMarker stop2 = new MapMarker(location, marker_img);
+        busStops.add(stop2);
+        map.addMapObject(stop2);
+
     }
 
     public void centerView (GeoCoordinate location){
@@ -464,61 +483,6 @@ public class MapActivity extends Activity {
         }
 
     }
-
-
-
-    public int arrivalEst () {
-
-        RouteManager rm = new RouteManager();
-        RoutePlan routePlan = new RoutePlan();
-        RouteOptions routeOptions = new RouteOptions();
-        routeOptions.setTransportMode(RouteOptions.TransportMode.CAR);
-        routeOptions.setHighwaysAllowed(false);
-        routeOptions.setRouteType(RouteOptions.Type.SHORTEST);
-        routeOptions.setRouteCount(1);
-        routePlan.setRouteOptions(routeOptions);
-
-
-        if (busLocation != null && closestStop(busStops) != null){
-            routePlan.addWaypoint(busLocation);
-            routePlan.addWaypoint(closestStop(busStops));
-
-
-
-
-        rm.calculateRoute(routePlan,
-                new RouteManager.Listener() {
-                    @Override
-                    public void onProgress(int i) {
-
-                    }
-
-                    @Override
-                    public void onCalculateRouteFinished(RouteManager.Error error, List<RouteResult> routeResults) {
-                        if (error == RouteManager.Error.NONE) {
-                            if (routeResults.get(0).getRoute() != null) {
-                                m_mapRoute = new MapRoute(routeResults.get(0).getRoute());
-                                m_mapRoute.setManeuverNumberVisible(true);
-                                arrTime = m_mapRoute.getRoute().getTta(Route.TrafficPenaltyMode.DISABLED, Route.WHOLE_ROUTE).getDuration();
-                                //map.addMapObject(m_mapRoute);
-
-                            } else {
-                                Log.e("Kyle", "Results are not valid");
-
-
-                            }
-                        } else {
-                            Log.e("Kyle", "Route calculation error");
-
-                        }
-                    }
-                });
-        }else{
-            Log.d("Kyle", "null waypoints");
-        }
-        return arrTime;
-    }
-    
 
     public GeoCoordinate closestStop(ArrayList<MapMarker> stops ) {
         double tempY1 = Math.abs(userLocation.getLatitude() - stops.get(0).getCoordinate().getLatitude());
