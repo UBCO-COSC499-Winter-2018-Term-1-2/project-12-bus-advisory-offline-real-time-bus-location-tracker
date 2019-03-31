e9b211aaa1b4ccc6c899f780db765b56b9bc1331
mattds9 <mattdeleeuw@hotmail.com>
2019-03-30 23:39:10 -0700

    Update config.js

--- development/rest-apis/here_maps/config.js
+++ development/rest-apis/here_maps/config.js
@@ -1,16 +1,26 @@
 var here_maps_config = {
-    app_id: 'B8XsbbQTez6ZoBejQzCC',
-    app_code: 'fWvulY1Z0dj0d1nPvF1oVA'
+  app_id: 'B8XsbbQTez6ZoBejQzCC',
+  app_code: 'fWvulY1Z0dj0d1nPvF1oVA'
 };
 var firebaseConfig = {
-    apiKey: "AIzaSyBibwa5zLw14GkjLekXsnisMM3Z7xbccQ8",
-    authDomain: "bus-advisory.firebaseapp.com",
-    databaseURL: "https://bus-advisory.firebaseio.com",
-    projectId: "bus-advisory",
-    storageBucket: "bus-advisory.appspot.com",
-    messagingSenderId: "691626187941"
-  };
+  type: "service_account",
+  project_id: "bus-advisory",
+  private_key_id: "9174486253e44b647e580c9a93b2c9a3c1ea4f4e",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCwjiUocWVc0JzU\nN9SBq58ZTHGaO0YBVdQD6ZyyBzkZ0pCP8pKhvD0atd2f035YmdoJQU9Qj/VL0BR1\nk2rTVEmGOxjoaxESWGC98BU8E4PzuZkNkO3YyElSgJwO7NlXXj+58BWz4GwLJKkQ\ncdpk5vGlqmffro6qqTgoXrcrRgoty+Vs15sbdeOIb+5Am3/m0FpZ+JR13T+a9tBC\nRQP4qlRt5U2cd5ddRKvVmh5UBOYHRPam2HgEBuzNgrUBADHjTf8JSoB+LchudlNu\nAYyZrTIHZiJ3wKMeh2iY/nG4JLbdtwhIbn8q3Ynd/UDTxoaAlp86q993OxEEQQts\nk07QreOFAgMBAAECggEADllLE02d+YxZxpSQAIbgbAeuruR01MxzKes9SSm1ng/j\nIFPMT3kzNR3FOIVRfiVo1zo31joQ6zuqwgB8XNb7j71zUYZMEaPdnuopa6PBZFg3\n5GacMfNbJCmgJUViB4v5HWUx+le5qDEvTU8Xev5a8sYV4ZO39A5DKmLCO7HaR2zp\nJfblrslbempoYrOkQiYBL2nR4cuCmBIsx6atrLwWJoB+yjHkT5PZ22m4kKsVnIkS\n3DRwNdzZ3Pdj05kyjLkQ13qtBV7FqFpPLdV74GhP2X1ZU6eoJO+e+mOVKWVl7g8W\n1/gzDw0BEy27Uc5kjoBjJsZHpBgOz9ZLVYVrWZs0SQKBgQDzUkkNwcoYuvwPuGMz\nFvuIKbVytAOTCfmeSJcjbNQA7YR9VeO2u8mwycZZMTeoQezNc8TYqTdmyda/81F9\nXRHMV0plfxBmw6jLkl4xk1TD2nkLoFV0fcFC3aoqlifYm0gfYTJS//DeFqdf+sXu\nJqY7VQn84FnRWoyHWX6f5FQjjQKBgQC5wUCGhDGQriT+ei8ZBWjs/PEb1wDpWegv\nvJeSZgvmGpunWiq80AvlcNqJPeDl4WF3X7zV1Sd4LkSkV8J58DjtNHHfXwMW2Y78\ncSIBc3ceaTyYe0HPD4Y9Fx2/O7kbR1KkErHX7HyOWtQhTKhha2B60M/fzsfT0yGN\ncZ9IE9IF2QKBgHHnKCmXeRrAlXRqdY2aCZ+BHt6WJEI13jNZed7seAYDKg1eIp4N\nJGps2jwwjQRn8AlxU0m1VSj5/VyX86LC6a0Fg98lbzEcEK1TVGLSvFB+ukFWCgrh\nP5IkoB8VcYu1jTqbTmqKQ2BDrJCCnRtR3mlR8r387C7Tu4s0utDwmu6VAoGBAJua\nYiYMyMRszLqLubZZDMsxG/x5P986HEEY11Ogvij0cx1eb9yY3s5FmhtLRYDsKsxn\n23S4yQZoo8GTbmrdK6Kwww8dtSXH+3pGfvj+M3O7LjKBVeBRHd9A5P9T1T0fov+k\nVWwOQ0ywk3CNmaN54mkfUHMn8cbk3v9whcBIMjAJAoGBAM2ggvnhvcfENLPkxv8f\nqvad9Vj0LkqZRJg7tINyGd2azLPmLsSbUKY0BkGGcYt5J4FMI80YFVvbMBs8gRGU\nwt00GgeAmMdZLep384KroHqrjVUolL+3p8CGieYo6Z2Hh7WragLXPM6HkGu9rGvM\nH6lIdk/9ndGag27MI3hmDZYY\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-7m1fj@bus-advisory.iam.gserviceaccount.com",
+  client_id: "115345171522781889914",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7m1fj%40bus-advisory.iam.gserviceaccount.com",
+  apiKey: "AIzaSyBibwa5zLw14GkjLekXsnisMM3Z7xbccQ8",
+  authDomain: "bus-advisory.firebaseapp.com",
+  databaseURL: "https://bus-advisory.firebaseio.com",
+  projectId: "bus-advisory",
+  storageBucket: "bus-advisory.appspot.com",
+  messagingSenderId: "691626187941"
+};
 module.exports = {
-    here_maps_config,
-    firebaseConfig
+  here_maps_config,
+  firebaseConfig
 }
